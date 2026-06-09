1ddf0306b5e9563c5937b8cc0cd4b230854d5b26
shabaz Pathan <shabazshaikh@hotmail.com>
2026-06-09 05:53:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const video = document.getElementById('intro-video');
 const btn = document.getElementById('exploreBtn');
 
-// Intro logic
+// 3 second logic
 video.addEventListener('timeupdate', () => {
     if (video.currentTime >= 3) btn.style.opacity = "1";
 });
@@ -11,6 +11,6 @@ function showContent() {
     document.getElementById('mainContent').style.display = 'block';
 }
 
-// Sliders Init
-new Swiper(".imgSwiper", { effect: "coverflow", grabCursor: true, centeredSlides: true, slidesPerView: "auto", loop: true, autoplay: { delay: 2500 }, coverflowEffect: { rotate: 40, depth: 100 } });
-new Swiper(".vidSwiper", { effect: "coverflow", grabCursor: true, centeredSlides: true, slidesPerView: "auto", loop: true, autoplay: { delay: 3500 }, coverflowEffect: { rotate: 40, depth: 100 } });
+// Swiper Init
+new Swiper(".imgSwiper", { effect: "coverflow", grabCursor: true, centeredSlides: true, slidesPerView: "auto", loop: true, coverflowEffect: { rotate: 40, depth: 100 } });
+new Swiper(".vidSwiper", { effect: "coverflow", grabCursor: true, centeredSlides: true, slidesPerView: "auto", loop: true, coverflowEffect: { rotate: 40, depth: 100 } });
